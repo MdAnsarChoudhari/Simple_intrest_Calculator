6a8c6c8b1d18097e61b964167147361770e326c4
Md Ansar choudhari <muhammadansarchoudhari@gmail.com>
2025-10-04 09:25:53 +0530

    Compound intrest added

--- SI.py
+++ SI.py
@@ -5,3 +5,10 @@ R=float(input("Enter the Rate of Intrest : "))
 SI=(P*T*R)/100
 
 print(f"The Simple Intrest Is {SI} ")
+
+
+A = P * (1 + R/100) ** T
+CI = A - P
+
+print(f"The Compound Interest is {CI}")
+print(f"The Total Amount is {A}")
